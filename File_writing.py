a2e9dda4da5c03422f7cf7fe66967a57df966bb4
Natalisckevich <107149114+Natalisckevich@users.noreply.github.com>
2022-10-16 17:18:02 +0300

    Update File_writing.py

--- File_writing.py
+++ File_writing.py
@@ -1,12 +1,12 @@
-из  User_interface  импортируйте  get_info  как  gi
+from User_interface import get_info as gi
 
-gi = info()
-writing_scv def ():
-    файл = 'Телефонная книга.csv'
-    открыть  с помощью (file, 'a', encoding = 'utf-8') в качестве  данных:
+info = gi()
+def writing_scv ():
+    file = 'Phonebook.csv'
+    with open (file, 'a', encoding = 'utf-8') as data:
         data.write(f'{info[0]};{info[1]};{info[2]};{info[3]}\n')
 
-writing_txt  определение ():
-    файл = 'Phonebook.txt '
-    открыть  с помощью (file, 'a', encoding = 'utf-8') в качестве  данных:
+def writing_txt ():
+    file = 'Phonebook.txt'
+    with open (file, 'a', encoding = 'utf-8') as data:
         data.write(f'Фамилия: {info[0]}\n\nИмя: {info[1]}\n\nНомер телефона: {info[2]}\n\nОписание: {info[3]}\n\n\n')
