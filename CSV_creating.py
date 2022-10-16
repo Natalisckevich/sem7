98b0ab8981102e9b3d74b678a607207335b90fac
Natalisckevich <107149114+Natalisckevich@users.noreply.github.com>
2022-10-16 17:16:19 +0300

    Update CSV_creating.py

--- CSV_creating.py
+++ CSV_creating.py
@@ -1,4 +1,4 @@
-создание  def ():
-    файл = 'Phonebook.csv'
-    открыть  с помощью (file, 'w', encoding = 'utf-8') в качестве  данных:
+def creating ():
+    file = 'Phonebook.csv'
+    with open (file, 'w', encoding = 'utf-8') as data:
         data.write(f'Фамилия;Имя;Номер телефона;Описание\n')
